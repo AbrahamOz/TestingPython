e8163fe7d7fb3c243bcdf1a562da4b65c503845f
Abraham Öz <abraham.oz@iver.se>
2022-12-26 13:13:47 +0100

    committing stuff

--- new 1.py
+++ new 1.py
@@ -3,3 +3,28 @@ print(5+5+5)
 print(6+6+6)
 print (7+7+7)
 print (8+8+8)
+
+
+# Define a class called "Person"
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  
+  def greet(self):
+    print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+# Create an object of the Person class
+person1 = Person("Alice", 30)
+
+# Access object attributes
+print(person1.name)  # Output: "Alice"
+print(person1.age)   # Output: 30
+
+# Call object method
+person1.greet()  # Output: "Hello, my name is Alice and I am 30 years old."
+
+# Create another object of the Person class
+person2 = Person("Bob", 35)
+person2.greet()  # Output: "Hello, my name is Bob and I am 35 years old."
+
